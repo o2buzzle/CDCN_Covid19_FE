bdfeb1ab5e89250a82d8acff7fd8be1c6a9ce6ec
Dangobums <62579923+Dangobums@users.noreply.github.com>
2022-03-14 22:17:01 +0700

    change for the better

--- my-app-1/src/App.js
+++ my-app-1/src/App.js
@@ -1,76 +1,30 @@
-import './App.css';
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { CssBaseline, Stack } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+import "./App.css";
+import * as React from "react";
+import MainPage from "./pages/MainPage";
+import Homepage from "./pages/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Link from '@mui/material/Link';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-    {'Copyright © '}
-    <Link color="inherit" href="https://mui.com/">
-      Your Website
-    </Link>{' '}
-    {new Date().getFullYear()}
-    {'.'}
-  </Typography>
-  );
-}
-const SearchButton = () => (
-  <Button variant = "contained" sx = {{}}>
-    Hello
-  </Button>
-  )
-
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center">
+//     {'Copyright © '}
+//     <Link color="inherit" href="https://mui.com/">
+//       Your Website
+//     </Link>{' '}
+//     {new Date().getFullYear()}
+//     {'.'}
+//   </Typography>
+//   );
+// }
 function App() {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Grid container>
-        <Grid item>
-          <TextField label="Input"/>
-          <TextField 
-          label="Output"
-          // InputProps={{
-          //   endAdornment: (
-          //     <InputAdornment position="end">
-          //       <SearchButton />
-          //     </InputAdornment>
-          //   ),
-          // }}
-        variant="standard"/>
-        </Grid>
-        <Grid item>
-          <Button variant="contained">Hello</Button>
-        </Grid>
-        <Stack direction="column"
-          justifyContent="center"
-          alignItems="flex-end"
-          spacing={2}>
-          <Item color="secondary">1</Item>
-          <Item>2</Item>
-          <Item>3</Item>
-          <Item>4</Item>
-          <Item>5</Item>
-
-         </Stack>
-      </Grid>
-     
-      <Copyright/>
-    </React.Fragment>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ my-app-1/src/pages/MainPage.js
@@ -0,0 +1,156 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import ButtonGroup from "@mui/material/ButtonGroup";
+
+const outerTheme = createTheme({
+  palette: {
+    number1: {
+      main: "#5300d9",
+    },
+    number2: {
+      main: "#6a5608",
+    },
+    number3: {
+      main: "#a3f48a",
+    },
+    number4: {
+      main: "#958bad",
+    },
+    number5: {
+      main: "#99d000",
+    },
+    number6: {
+      main: "#70005a",
+    },
+    number7: {
+      main: "#00b994",
+    },
+    number8: {
+      main: "#3ad9f1",
+    },
+    number9: {
+      main: "#4d000c",
+    },
+    number10: {
+      main: "#e4b84a",
+    },
+  },
+});
+
+const node = [
+  {
+    value: "PATIENT_ID",
+    color: "number1",
+  },
+  {
+    value: "NAME",
+    color: "number2",
+  },
+  {
+    value: "AGE",
+    color: "number3",
+  },
+  {
+    value: "GENDER",
+    color: "number4",
+  },
+  {
+    value: "JOB",
+    color: "number5",
+  },
+  {
+    value: "ORGANIZATION",
+    color: "number6",
+  },
+  {
+    value: "LOCATION",
+    color: "number7",
+  },
+  {
+    value: "DATE",
+    color: "number8",
+  },
+  {
+    value: "SYMPTOM_AND_DISEASE",
+    color: "number9",
+  },
+  {
+    value: "TRANPORTATION",
+    color: "number10",
+  },
+];
+
+function MainPage() {
+  const [input, setInput] = React.useState("");
+  const [output, setOutput] = React.useState("");
+
+  const onTextInput = (e) => {
+    setInput(e.target.value);
+  };
+  const onButtonClick = () => {
+    setOutput(input);
+  };
+  return (
+    <React.Fragment>
+      <Typography variant="h1" color="text.secondary" align="center">
+        Hello
+      </Typography>
+      <Grid container justifyContent="center" spacing="1">
+        <Grid item xs={4}>
+          <TextField
+            multiline
+            rows="5"
+            label="Input"
+            fullWidth
+            value={input}
+            onChange={onTextInput}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <TextField
+            label="Output"
+            multiline
+            rows="5"
+            fullWidth
+            value={output}
+          />
+        </Grid>
+        <Grid item xs>
+          <ThemeProvider theme={outerTheme}>
+            <ButtonGroup
+              sx={{ "& button": { m: 0.5 } }}
+              variant="contained"
+              size="medium"
+              disableElevation
+              orientation="vertical"
+              aria-label="vertical outlined button group"
+            >
+              {node.map((item) => (
+                <Button key={item.value} color={item.color}>
+                  {" "}
+                  {item.value}{" "}
+                </Button>
+              ))}
+            </ButtonGroup>
+          </ThemeProvider>
+        </Grid>
+      </Grid>
+      <Button
+        variant="contained"
+        size="small"
+        disabled={!input}
+        onClick={() => {
+          onButtonClick();
+        }}
+      >
+        Hello
+      </Button>
+    </React.Fragment>
+  );
+}
+
+export default MainPage;
