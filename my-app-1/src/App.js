d9965410e3a6fe2a9ca417912ccb5a6f73f9d3ba
Dangobums <62579923+Dangobums@users.noreply.github.com>
2022-03-24 19:10:59 +0700

    cleaning up redundant file

--- my-app-1/src/App.js
+++ my-app-1/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import * as React from "react";
 import MainPage from "././pages/MainPage";
 import Homepage from "././pages/Homepage";
@@ -6,33 +5,18 @@ import Pricing from "./pages/Pricing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/components/Layout";
 
-// function Copyright() {
-//   return (
-//     <Typography variant="body2" color="text.secondary" align="center">
-//     {'Copyright © '}
-//     <Link color="inherit" href="https://mui.com/">
-//       Your Website
-//     </Link>{' '}
-//     {new Date().getFullYear()}
-//     {'.'}
-//   </Typography>
-//   );
-// }
-// function Dango() => {
-//	console.log("Hello")
-// }
 function App() {
-	return (
-		<BrowserRouter>
-			<Layout>
-				<Routes>
-					<Route path="/" element={<Homepage />} />
-					<Route path="/create" element={<MainPage />} />
-					<Route path="/price" element={<Pricing />} />
-				</Routes>
-			</Layout>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/create" element={<MainPage />} />
+          <Route path="/price" element={<Pricing />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- my-app-1/src/pages/Homepage.js
+++ my-app-1/src/pages/Homepage.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import GlobalStyles from "@mui/material/GlobalStyles";
+import { CssBaseline } from "@mui/material";
 function Homepage() {
-	return <div>Home</div>;
+  return (
+    <React.Fragment>
+      <GlobalStyles
+        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+      />
+      <CssBaseline />
+      <div>Hello</div>
+    </React.Fragment>
+  );
 }
 
 export default Homepage;
