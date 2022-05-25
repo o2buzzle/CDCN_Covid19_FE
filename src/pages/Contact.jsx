b79439dd63d44ea9f4e793055128dec026307708
o2buzzle <76864037+o2buzzle@users.noreply.github.com>
2022-05-25 19:22:26 +0700

    fix: Feedback

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,6 +8,14 @@ import {
 } from "@mui/material";
 import React from "react";
 
+function submitToTeam() {
+  // Do nothing, just like university forms
+  /* eslint-disable no-alert */
+  alert(
+    "Thank you. Your feedback has been recorded.\n\nWe will get back to you soon."
+  );
+}
+
 export default function Contact() {
   return (
     <div>
@@ -46,7 +54,7 @@ export default function Contact() {
             >
               Get in touch
             </Typography>
-            <form>
+            <form onSubmit={submitToTeam}>
               <Grid container spacing={1}>
                 <Grid xs={12} sm={6} item>
                   <TextField
